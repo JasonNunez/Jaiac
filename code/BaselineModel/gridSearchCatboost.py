a13c425cfbaca41221b25173e932ae3e5925a8b4
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-03-27 09:58:15 -0400

    Add a grid search model to model the catboost

--- code/BaselineModel/gridSearchCatboost.py
+++ code/BaselineModel/gridSearchCatboost.py
@@ -1,14 +1,11 @@
 import re
 
-from sklearn.model_selection import GridSearchCV, KFold
+from catboost import CatBoostClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV, KFold
 import pandas as pd
 import time
-
-from sklearn.tree import DecisionTreeClassifier
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 # Set option to display more rows
 pd.set_option('display.max_rows', 500)
@@ -49,7 +46,7 @@ y = df['label']
 # Set up the pipeline
 pipeline = Pipeline([
     ('tfidf', TfidfVectorizer(stop_words='english')),
-    ('decisiontree', DecisionTreeClassifier())
+    ('catboost', CatBoostClassifier(task_type='GPU', verbose=0)) # Set verbose=0 for less output during training
 ])
 
 # Define the parameter grid
@@ -58,15 +55,14 @@ param_grid = {
     'tfidf__binary': [True],
     'tfidf__use_idf': [True, False],
     'tfidf__stop_words': ['english'],
-    'tfidf__ngram_range': [(1, 1), (1, 2), (1, 3)],
-    'decisiontree__max_depth': [10, 50, 100, 200],
-    'decisiontree__min_samples_split': [2, 50, 100],
-    'decisiontree__min_samples_leaf': [1, 20, 50],
-    'decisiontree__class_weight': [None, 'balanced']
+    'tfidf__ngram_range': [(1, 1)],
+    'catboost__iterations': [100, 500, 1000, 2000],  # Number of trees.
+    'catboost__learning_rate': [0.01, 0.05, 0.1, 0.2],  # Step size shrinkage used to prevent overfitting.
+    'catboost__depth': [6, 10, 20, 30],  # Depth of each tree.
 }
 
 # Set up GridSearchCV
-grid_search = GridSearchCV(pipeline, param_grid, scoring='accuracy', cv=KFold(n_splits=5, shuffle=True, random_state=3270), verbose=3, n_jobs=-1)
+grid_search = GridSearchCV(pipeline, param_grid, scoring='accuracy', cv=KFold(n_splits=5, shuffle=True, random_state=3270), verbose=3)
 
 # Track the start time
 start_time = time.time()
@@ -76,11 +72,6 @@ grid_search.fit(X, y)
 
 # Calculate the duration
 duration = time.time() - start_time
-
-# Print the best parameters and their corresponding accuracy
-print(f"Best parameters: {grid_search.best_params_}")
-print(f"Best accuracy: {grid_search.best_score_}")
-print(f"Time taken: {duration} seconds")
 
 # If you want the top 5 configurations:
 results = pd.DataFrame(grid_search.cv_results_)
