f290f4358514909ba630973fd6ea4c97479a267f
Jason Nunez <55280347+JasonNunez@users.noreply.github.com>
2024-03-31 23:46:16 -0400

    Update model_evaluation to use top 5 decision tree configurations

--- code/BaselineModel/model_evaluation.py
+++ code/BaselineModel/model_evaluation.py
@@ -9,14 +9,12 @@ model through cross-validation.
 """
 
 import time
-
-import numpy as np
 from catboost import CatBoostClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score, KFold
 from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
 
 from prepare_data import load_and_clean_data
 
@@ -83,17 +81,13 @@ def train_and_evaluate_model_cat(configuration, train_data, train_labels, test_d
             iterations=configuration.get('catboost__iterations', 1000),
             learning_rate=configuration.get('catboost__learning_rate', 0.2),
             depth=configuration.get('catboost__depth', 6),
-            verbose=0,
             task_type='GPU' if configuration.get('catboost__use_gpu', False) else 'CPU'
         ))
     ])
 
     start_time = time.time()
-    # Train the model on the training data
     pipeline.fit(train_data, train_labels)
-    # Predict on the testing data
     predictions = pipeline.predict(test_data)
-    # Calculate accuracy
     accuracy = accuracy_score(test_labels, predictions)
     end_time = time.time()
 
@@ -101,6 +95,46 @@ def train_and_evaluate_model_cat(configuration, train_data, train_labels, test_d
     print(f"Test accuracy: {accuracy}")
     print(f"Time taken: {end_time - start_time} seconds")
     print("---------------------------------------------------------")
+
+
+def train_and_evaluate_model_decision(configuration, train_data, train_labels, test_data, test_labels):
+    """
+    Trains the model with a Decision Tree on the training dataset and evaluates it on the testing dataset.
+
+    Args:
+        configuration (dict): Configuration for the TF-IDF vectorizer and Decision Tree classifier.
+        train_data (pandas.Series): The text data to train on.
+        train_labels (pandas.Series): The labels for the training data.
+        test_data (pandas.Series): The text data to test on.
+        test_labels (pandas.Series): The labels for the testing data.
+    """
+    pipeline = Pipeline([
+        ('tfidf', TfidfVectorizer(
+            binary=configuration.get('tfidf__binary', True),
+            max_features=configuration.get('tfidf__max_features', None),
+            ngram_range=configuration.get('tfidf__ngram_range', (1, 1)),
+            stop_words=configuration.get('tfidf__stop_words', 'english'),
+            use_idf=configuration.get('tfidf__use_idf', True)
+        )),
+        ('decision_tree', DecisionTreeClassifier(
+            max_depth=configuration.get('decisiontree__max_depth', None),
+            min_samples_split=configuration.get('decisiontree__min_samples_split', 2),
+            min_samples_leaf=configuration.get('decisiontree__min_samples_leaf', 1),
+            class_weight=configuration.get('decisiontree__class_weight', None)
+        ))
+    ])
+
+    start_time = time.time()
+    pipeline.fit(train_data, train_labels)
+    predictions = pipeline.predict(test_data)
+    accuracy = accuracy_score(test_labels, predictions)
+    end_time = time.time()
+
+    print(f"Running configuration: {configuration}")
+    print(f"Test accuracy: {accuracy}")
+    print(f"Time taken: {end_time - start_time} seconds")
+    print("---------------------------------------------------------")
+
 
 
 def main():
@@ -218,13 +252,75 @@ def main():
         }
     }
 
+    decisiontree_configurations = {
+        1: {
+            'decisiontree__class_weight': None,
+            'decisiontree__max_depth': 200,
+            'decisiontree__min_samples_leaf': 20,
+            'decisiontree__min_samples_split': 100,
+            'tfidf__binary': True,
+            'tfidf__max_features': 2000,
+            'tfidf__ngram_range': (1, 3),
+            'tfidf__stop_words': 'english',
+            'tfidf__use_idf': False
+        },
+        2: {
+            'decisiontree__class_weight': None,
+            'decisiontree__max_depth': 200,
+            'decisiontree__min_samples_leaf': 20,
+            'decisiontree__min_samples_split': 100,
+            'tfidf__binary': True,
+            'tfidf__max_features': 2000,
+            'tfidf__ngram_range': (1, 2),
+            'tfidf__stop_words': 'english',
+            'tfidf__use_idf': False
+        },
+        3: {
+            'decisiontree__class_weight': None,
+            'decisiontree__max_depth': 200,
+            'decisiontree__min_samples_leaf': 50,
+            'decisiontree__min_samples_split': 100,
+            'tfidf__binary': True,
+            'tfidf__max_features': 2000,
+            'tfidf__ngram_range': (1, 2),
+            'tfidf__stop_words': 'english',
+            'tfidf__use_idf': False
+        },
+        4: {
+            'decisiontree__class_weight': None,
+            'decisiontree__max_depth': 200,
+            'decisiontree__min_samples_leaf': 2,
+            'decisiontree__min_samples_split': 100,
+            'tfidf__binary': True,
+            'tfidf__max_features': 2000,
+            'tfidf__ngram_range': (1, 2),
+            'tfidf__stop_words': 'english',
+            'tfidf__use_idf': False
+        },
+        5: {
+            'decisiontree__class_weight': None,
+            'decisiontree__max_depth': 200,
+            'decisiontree__min_samples_leaf': 100,
+            'decisiontree__min_samples_split': 100,
+            'tfidf__binary': True,
+            'tfidf__max_features': 2000,
+            'tfidf__ngram_range': (1, 3),
+            'tfidf__stop_words': 'english',
+            'tfidf__use_idf': False
+        }
+    }
+
     for config_number, config_params in log_configurations.items():
         print(f"Running configuration number {config_number}")
         train_and_evaluate_model_log(config_params, train_x, train_y, test_x, test_y)
 
     for config_number, config_params in catboost_configurations.items():
         print(f"Running configuration number {config_number}")
-        train_and_evaluate_model_cat(config_params, train_x, train_y, test_x)
+        train_and_evaluate_model_cat(config_params, train_x, train_y, test_x, test_y)
+
+    for config_number, config_params in decisiontree_configurations.items():
+        print(f"Running configuration number {config_number}")
+        train_and_evaluate_model_decision(config_params, train_x, train_y, test_x, test_y)
 
 
 if __name__ == '__main__':
